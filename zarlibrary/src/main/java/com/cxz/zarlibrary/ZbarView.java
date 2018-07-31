e4edddd4dcb8db1983cd2297d99d65cd220817e8
chenxz <563859095@qq.com>
2018-07-31 15:20:45 +0800

    commit

--- zarlibrary/src/main/java/com/cxz/zarlibrary/ZbarView.java
+++ zarlibrary/src/main/java/com/cxz/zarlibrary/ZbarView.java
@@ -1,7 +1,150 @@
 package com.cxz.zarlibrary;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Rect;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+
+import com.cxz.code.core.BarcodeType;
+import com.cxz.code.core.QRCodeView;
+import com.cxz.code.core.ScanResult;
+
+import net.sourceforge.zbar.Config;
+import net.sourceforge.zbar.Image;
+import net.sourceforge.zbar.ImageScanner;
+import net.sourceforge.zbar.Symbol;
+import net.sourceforge.zbar.SymbolSet;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Created by chenxz on 2018/7/28.
  */
-public class ZbarView {
+public class ZbarView extends QRCodeView {
+
+    static {
+        System.loadLibrary("iconv");
+    }
+
+    private ImageScanner mScanner;
+    private List<BarcodeFormat> mFormatList;
+
+    public ZbarView(Context context) {
+        this(context, null);
+    }
+
+    public ZbarView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public ZbarView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+    @Override
+    protected void setupReader() {
+        mScanner = new ImageScanner();
+        mScanner.setConfig(0, Config.X_DENSITY, 3);
+        mScanner.setConfig(0, Config.Y_DENSITY, 3);
+        mScanner.setConfig(Symbol.NONE, Config.ENABLE, 0);
+
+        for (BarcodeFormat format : getFormats()) {
+            mScanner.setConfig(format.getId(), Config.ENABLE, 1);
+        }
+    }
+
+    public Collection<BarcodeFormat> getFormats() {
+        if (mBarcodeType == BarcodeType.ONE_DIMENSION) {
+            return BarcodeFormat.ONE_DIMENSION_FORMAT_LIST;
+        } else if (mBarcodeType == BarcodeType.TWO_DIMENSION) {
+            return BarcodeFormat.TWO_DIMENSION_FORMAT_LIST;
+        } else if (mBarcodeType == BarcodeType.ONLY_QR_CODE) {
+            return Collections.singletonList(BarcodeFormat.QRCODE);
+        } else if (mBarcodeType == BarcodeType.ONLY_CODE_128) {
+            return Collections.singletonList(BarcodeFormat.CODE128);
+        } else if (mBarcodeType == BarcodeType.ONLY_EAN_13) {
+            return Collections.singletonList(BarcodeFormat.EAN13);
+        } else if (mBarcodeType == BarcodeType.HIGH_FREQUENCY) {
+            return BarcodeFormat.HIGH_FREQUENCY_FORMAT_LIST;
+        } else if (mBarcodeType == BarcodeType.CUSTOM) {
+            return mFormatList;
+        } else {
+            return BarcodeFormat.ALL_FORMAT_LIST;
+        }
+    }
+
+    /**
+     * 设置识别的格式
+     *
+     * @param barcodeType 识别的格式
+     * @param formatList  barcodeType 为 BarcodeType.CUSTOM 时，必须指定该值
+     */
+    public void setType(BarcodeType barcodeType, List<BarcodeFormat> formatList) {
+        mBarcodeType = barcodeType;
+        mFormatList = formatList;
+
+        if (mBarcodeType == BarcodeType.CUSTOM && (mFormatList == null || mFormatList.isEmpty())) {
+            throw new RuntimeException("barcodeType 为 BarcodeType.CUSTOM 时 formatList 不能为空");
+        }
+        setupReader();
+    }
+
+    @Override
+    protected ScanResult processData(byte[] data, int width, int height, boolean isRetry) {
+        Image barcode = new Image(width, height, "Y800");
+
+        Rect scanBoxAreaRect = mScanBoxView.getScanBoxAreaRect(height);
+        if (scanBoxAreaRect != null && !isRetry && scanBoxAreaRect.left + scanBoxAreaRect.width() <= width
+                && scanBoxAreaRect.top + scanBoxAreaRect.height() <= height) {
+            barcode.setCrop(scanBoxAreaRect.left, scanBoxAreaRect.top, scanBoxAreaRect.width(), scanBoxAreaRect.height());
+        }
+        barcode.setData(data);
+        String result = processData(barcode);
+        return new ScanResult(result);
+    }
+
+    @Override
+    protected ScanResult processBitmapData(Bitmap bitmap) {
+        try {
+            int picWidth = bitmap.getWidth();
+            int picHeight = bitmap.getHeight();
+            Image barcode = new Image(picWidth, picHeight, "RGB4");
+            int[] pix = new int[picWidth * picHeight];
+            bitmap.getPixels(pix, 0, picWidth, 0, 0, picWidth, picHeight);
+            barcode.setData(pix);
+            String result = processData(barcode.convert("Y800"));
+            return new ScanResult(result);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private String processData(Image barcode) {
+        String result = null;
+        if (mScanner.scanImage(barcode) != 0) {
+            SymbolSet symbolSet = mScanner.getResults();
+            for (Symbol symbol : symbolSet) {
+                String symData;
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
+                    symData = new String(symbol.getDataBytes(), StandardCharsets.UTF_8);
+                } else {
+                    symData = symbol.getData();
+                }
+                if (!TextUtils.isEmpty(symData)) {
+                    if (isShowLocationPoint()) {
+                        transformToViewCoordinates(symbol.getLocationPoints(), null);
+                    }
+                    result = symData;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
 }
